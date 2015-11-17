12c4a43f86bbfe27ef11f2d9c44602af80fd9f44
Tyler Long <tyler4long@gmail.com>
2015-11-17 16:54:51 +0800

    v1.6.0

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,9 @@ from fabric.api import local
 
 
 def update():
-    local('rm -rf vendor')
-    local('bower cache clean')
-    local('bower update')
-    local('rm vendor/markdown-plus/bower.json')
-    local('mv vendor/markdown-plus/* .')
-    local('rm -rf vendor')
+    local('rm -rf dist')
+    local('cp -r ../markdown-plus/dist .')
+    local('cp ../markdown-plus/index.html .')
+    local('cp ../markdown-plus/index.js .')
+    local('cp ../markdown-plus/sample.md .')
+    local('cp ../markdown-plus/icon.png .')
